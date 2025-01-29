f93bef3a504bd224424535aaa77cbca69726f0dc
mpan <Matti.Panula@seamk.fi>
2025-01-29 15:35:53 +0200

    added devcontainer stuff

---
+++ src/main/java/com/seamk/springdockertest/HelloController.java
@@ -0,0 +1,14 @@
+package com.seamk.springdockertest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
